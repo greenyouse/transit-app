973138f8a4f8c282311bc9b00661bc943924f30e
greenyouse <greenyouse@hotmail.com>
2016-06-13 23:44:20 -0500

    fix: improve sw caching and offline detection
    - added event listeners for online/offline in transit-app.html to
      dynamically switch between offline and online mode
    - added images and /src files so that the app shell could be served out
      of the service worker's cache in offline mode

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,9 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    'src/*',
+    '/images/*'
   ],
   navigateFallback: '/index.html'
 };
